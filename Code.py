930614b2ebcb20f01de501a3121076ef59abe7b6
Ravi20205 <nuggehalliravi@gmail.com>
2025-06-16 17:31:25 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,20 @@
+def calc(op, x, y):
+    if op == "add":
+        return x + y
+    elif op == "subtract":
+        return x - y
+    elif op == "multiply":
+        return x * y
+    elif op == "divide":
+        if y == 0:
+            print("Error: Division by zero")
+        else:
+            x / y  # Bug: forgot return
+    else:
+        print("Invalid operation")
+
+# Test cases
+print(calc("add", 2, 3))        # Should be 5
+print(calc("divide", 10, 0))    # Should show error
+print(calc("divide", 10, 2))    # Should be 5.0, but returns None
+print(calc("power", 2, 3))      # Invalid op test
